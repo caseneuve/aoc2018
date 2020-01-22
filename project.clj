239df52049371d845a47246a71e56b4c37800a62
Piotr Kaznowski <piotrkaznowski@gmail.com>
2020-01-22 15:56:39 +0100

    initializes project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aoc18 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2018 while learning Clojure"
+  :url "https://gitlab.com/pkaznowski/advent_of_code_2018"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot aoc18
+  :profiles {:uberjar {:aot :all}})
